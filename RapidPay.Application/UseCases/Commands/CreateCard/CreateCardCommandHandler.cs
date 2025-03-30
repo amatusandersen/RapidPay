5695a88a19ac40320babbf6e6349989284c066e2
Aleh Matus <a.matus@andersenlab.com>
2025-03-30 18:05:58 +0200

    fix: add card number uniqueness check

--- RapidPay.Application/UseCases/Commands/CreateCard/CreateCardCommandHandler.cs
+++ RapidPay.Application/UseCases/Commands/CreateCard/CreateCardCommandHandler.cs
@@ -1,8 +1,10 @@
 ﻿using MediatR;
 using RapidPay.Application.Dtos;
 using RapidPay.Application.Mappings;
+using RapidPay.Domain.Exceptions.Cards;
 using RapidPay.Domain.Interfaces.Factories;
 using RapidPay.Domain.Interfaces.Infrastructure;
+using RapidPay.Infrastructure.Persistence.Specifications.Cards;
 
 namespace RapidPay.Application.UseCases.Commands.CreateCard
 {
@@ -11,6 +13,12 @@ namespace RapidPay.Application.UseCases.Commands.CreateCard
         public async Task<CardModel> Handle(CreateCardCommand request, CancellationToken cancellationToken)
         {
             var card = cardFactory.Create(request.InitialBalance, request.CreditLimit);
+
+            var cardSpecification = new GetCardByNumberSpecification(card.Number);
+            if (await unitOfWork.CardRepository.ExistsAsync(cardSpecification.Criteria))
+            {
+                throw new CardNumberAlreadyAssignedException(card.Number);
+            }
 
             await unitOfWork.CardRepository.AddAsync(card);
             await unitOfWork.SaveChangesAsync();
---
+++ RapidPay.Domain/Exceptions/Cards/CardNumberAlreadyAssignedException.cs
@@ -0,0 +1,8 @@
+﻿namespace RapidPay.Domain.Exceptions.Cards
+{
+    public class CardNumberAlreadyAssignedException : Exception
+    {
+        public CardNumberAlreadyAssignedException() : base("Card number is already assigned to another card.") { }
+        public CardNumberAlreadyAssignedException(string cardNumber) : base($"Card number is already assigned to another card (card number: {cardNumber}).") { }
+    }
+}
